6745dc141b90dc5f62a5a65cfb6a8d8efaee6b9b
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2022-09-08 00:36:15 -0300

    Adicionei a tela de login mas ainda falta coisa

--- src/Components/Sistema/Login/Login.jsx
+++ src/Components/Sistema/Login/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import '../../../css/Login.css'
+import Input from './Input';
 
 const Login = ()=>{
     return (
         <>
-        
+            <section className="login">
+                <div className="foto">
+
+                </div>
+                <div className="form">
+                    <form action="">
+                        <Input 
+                            type='text'
+                            nome='login'
+                            label='Label'
+                        />
+                        <Input 
+                            type='password'
+                            nome='senha'
+                            label='Senha'
+                        />
+                        <a href="">Esqueci a senha.</a>
+                        <input type="submit" value="Logar" />
+                        <input type="submit" value="Cadastrar" />
+                    </form>
+                </div>
+            </section>
         </>
     )
 };
