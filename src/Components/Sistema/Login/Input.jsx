d6ba478f74473e1ddb6e526fecc58948accd9ff9
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2022-09-08 00:36:27 -0300

    Adicionei a tela de login mas ainda falta coisa

---
+++ src/Components/Sistema/Login/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../../css/Input.css'
+
+const Input = (props)=>{
+    return(
+        <>
+            <div className="inputSingle">
+                <input type={props.type} id={props.nome} name={props.nome} className='inputText' required />
+                <label htmlFor={props.nome}>{props.label}</label>
+            </div>
+        </>
+    )
+};
+
+export default Input;
