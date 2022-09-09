37bd151f8d37a063f9dacc0cba4f9b78b438563c
Vinicius Oliveira <vinioliveiracontato03@gmail.com>
2022-09-09 20:14:28 -0300

    Tela do recrutador para cadastro

--- src/Components/Sistema/Recrutador/Recrutador.jsx
+++ src/Components/Sistema/Recrutador/Recrutador.jsx
@@ -1,17 +1,87 @@
 import React from "react";
 import '../../../css/Recrutador.css'
+import CheckBox from "../../CheckBox";
+import Input from "../../Input";
+import Submit from "../../Submit";
+import TextArea from "../../TextArea";
 
 const Recrutador = ()=>{
     return(
         <>
         <section className="recrutador">
             <div className="container">
-                <h1>Recrutador</h1>
-                <ul>
-                    <li>Cadastro de processos seletivos</li>
-                    <li>Cadastro do contato do Recrutador</li>
-                    <li>Cadastro de desafio do candidato</li>
-                </ul>
+                <div className="box">
+                    <h3>Cadastro de processos seletivos</h3>
+                    <form action="">
+                        <Input
+                            type='text'
+                            nome='nomeVaga'
+                            label='Nome da vaga'
+                            cor='white'
+                        />
+                        <Input
+                            type='text'
+                            nome='areaVaga'
+                            label='Área da vaga'
+                            cor='white'
+                        />
+                        <Input
+                            type='number'
+                            nome='salarioVaga'
+                            label='Salário da vaga'
+                            cor='white'
+                        />
+                        <Input
+                            type='text'
+                            nome='desafioVaga'
+                            label='Desafio da vaga'
+                            cor='white'
+                        />
+                        <Input
+                            type='number'
+                            nome='modalidadeVaga'
+                            label='Modalidade da vaga'
+                            cor='white'
+                        />
+                        <TextArea
+                            nome='descricaoArea'
+                            placeholder='Digite a descrição da vaga'
+                        />
+                        <TextArea
+                            nome='beneficiosArea'
+                            placeholder='Digite os benefícios da vaga'
+                        />
+                        <CheckBox
+                            nome='pcd'
+                            label='PCD Aplicável'
+                        />
+                        <Submit
+                            destino='/'
+                            valor='Enviar'
+                        />
+                    </form>
+                </div>
+                <div className="box">
+                    <h3>Cadastro de desafio do candidato</h3>
+                    <form action="">
+                        <Input
+                            type='text'
+                            nome='nomeDesafio'
+                            label='Nome do desafio'
+                            cor='white'
+                        />
+                        <Input
+                            type='number'
+                            nome='tempoDesafio'
+                            label='Tempo do desafio'
+                            cor='white'
+                        />
+                        <Submit
+                            destino='/'
+                            valor='Enviar'
+                        />
+                    </form>
+                </div>
             </div>
         </section>
         </>
